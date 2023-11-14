fadae47e7f994c30f29c96d701282d1836c548dc
oyazigi <mambo0001@outlook.com>
2023-11-14 04:04:14 -0300

    arrumando identação

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 window.addEventListener('yt-page-data-updated', function () {
-    const button = document.querySelector(".ytp-ad-skip-button-text");
-    const timer = document.querySelector(".ytp-ad-preview-text-modern");
-    let count = 0;
-  
-    const videoBeginningInterval = setInterval(() => {
-      count++;
-      if (count === 2) {
-        checkAndSkipADS(button, timer);
-        clearInterval(videoBeginningInterval);
-      }
-    }, 1000);
-  
-    const afterBeginningInterval = setInterval(() => {
+  const button = document.querySelector(".ytp-ad-skip-button-text");
+  const timer = document.querySelector(".ytp-ad-preview-text-modern");
+  let count = 0;
+
+  const videoBeginningInterval = setInterval(() => {
+    count++;
+    if (count === 2) {
       checkAndSkipADS(button, timer);
-    }, 5000);
-  
+      clearInterval(videoBeginningInterval);
+    }
+  }, 1000);
+
+  const afterBeginningInterval = setInterval(() => {
+    checkAndSkipADS(button, timer);
+  }, 5000);
+
   function checkAndSkipADS(button, timer) {
     if (button && timer) {
       const jumpADInterval = setInterval(() => {
@@ -26,4 +26,3 @@ window.addEventListener('yt-page-data-updated', function () {
     }
   }
 });
-  
