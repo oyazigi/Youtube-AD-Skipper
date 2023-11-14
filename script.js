1fa497ff07f381a19ef29886ed8fb9496499ebf2
oyazigi <mambo0001@outlook.com>
2023-11-14 03:00:48 -0300

    atualizando para a nova versao do youtube e adicionando feature de pular ads no meio do video

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-    window.addEventListener('yt-page-data-updated', function () {
-        var timerInterval = setInterval(() => {
-            let button = document.querySelector(".ytp-ad-skip-button-text");
-            let timer = document.querySelector(".ytp-ad-preview-text");
-            if(button && timer){
-                if (timer.offsetWidth <= 0 || timer.offsetHeight <= 0) {
-                    button.click();
-                    clearInterval(timerInterval);
-                }
-            }
-            else{
-                    clearInterval(timerInterval);
-            } 
-        }, 500);
-    });
-    
-
+window.addEventListener('yt-page-data-updated', function () {
+    const button = document.querySelector(".ytp-ad-skip-button-text");
+    const timer = document.querySelector(".ytp-ad-preview-text-modern");
+    let count = 0;
+  
+    const videoBeginningInterval = setInterval(() => {
+      count++;
+      if (count === 2) {
+        checkAndSkipADS(button, timer);
+        clearInterval(videoBeginningInterval);
+      }
+    }, 1000);
+  
+    const afterBeginningInterval = setInterval(() => {
+      checkAndSkipADS(button, timer);
+    }, 5000);
+  });
+  
+  function checkAndSkipADS(button, timer) {
+    if (button && timer) {
+      const jumpADInterval = setInterval(() => {
+        if (timer.offsetWidth <= 0 || timer.offsetHeight <= 0) {
+          button.click();
+          clearInterval(jumpADInterval);
+        }
+      }, 200);
+    }
+  }
+  
