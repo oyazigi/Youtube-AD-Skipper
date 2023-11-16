8399b3bb5957dbf2eaf14263d4cbd0fe991bc0f2
oyazigi <mambo0001@outlook.com>
2023-11-16 09:15:41 -0300

    Melhorando o código

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 window.addEventListener('yt-page-data-updated', function () {
-  const button = document.querySelector(".ytp-ad-skip-button-text");
+  const button_text = document.querySelector(".ytp-ad-skip-button-text");
   const timer = document.querySelector(".ytp-ad-preview-text-modern");
-  const button_container = document.querySelector(".ytp-ad-preview-container");
-  let count = 0;
+  const buttonIcon = document.querySelector(".ytp-ad-skip-button-icon-modern");
 
-  const videoBeginningInterval = setInterval(() => {
-    count++;
-    if (count === 2) {
-      checkAndSkipADS(button, timer);
-      clearInterval(videoBeginningInterval);
-    }
+  const adblockInterval = setInterval(() => {
+    checkAndSkipADS(button_text, timer);
   }, 1000);
-
-  const afterBeginningInterval = setInterval(() => {
-    checkAndSkipADS(button, timer);
-  }, 4000);
 
   function checkAndSkipADS(button, timer) {
     if(timer!== null){
-      if (button_container.offsetWidth > 0) {
+      if (buttonIcon.offsetWidth > 0) {
         const jumpADInterval = setInterval(() => {
           if (timer.offsetWidth <= 0 || timer.offsetHeight <= 0) {
             button.click();
