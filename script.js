0613fcf78fcae72eed29b5f759ac8333308789c7
oyazigi <mambo0001@outlook.com>
2023-11-16 20:44:02 -0300

    Add skip banners feature

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 window.addEventListener('yt-page-data-updated', function () {
-  const button_text = document.querySelector(".ytp-ad-skip-button-text");
-  const timer = document.querySelector(".ytp-ad-preview-text-modern");
-  const buttonIcon = document.querySelector(".ytp-ad-skip-button-icon-modern");
+  let buttonIcon = document.querySelector(".ytp-ad-skip-button-icon-modern");
 
   const adblockInterval = setInterval(() => {
-    checkAndSkipADS(button_text, timer);
-  }, 1000);
+    checkAndSkipADS(buttonIcon);
+  }, 100);
 
-  function checkAndSkipADS(button, timer) {
-    if(timer!== null){
-      if (buttonIcon.offsetWidth > 0) {
-        const jumpADInterval = setInterval(() => {
-          if (timer.offsetWidth <= 0 || timer.offsetHeight <= 0) {
-            button.click();
-            clearInterval(jumpADInterval);
-          }
-        }, 200);
+  function checkAndSkipADS(button) {
+    if (button) {
+      if (button.offsetWidth > 0) {
+        button.click();
       }
     }
+
+    const statementBanner = document.querySelector(".ytd-statement-banner-renderer");
+    if (statementBanner) {
+      const closeButton = document.getElementsByClassName("yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default");
+      if (closeButton.length > 0) {
+        closeButton[0].click();
+      }
+    }
+
+    const banners = document.querySelectorAll('[class*="masthead-ad"]');
+    banners.forEach(banner => {
+      if (banner) {
+        banner.style.display = 'none';
+      }
+    });
+
+    const moreAds = document.getElementsByClassName("style-scope ytd-in-feed-ad-layout-renderer sparkles-light-cta");
+    if (moreAds.length > 0) {
+      moreAds[0].style.display = 'none';
+    }
+
+    const evenMoreAds = document.querySelectorAll('[class*="ytd-action-companion-ad-renderer"]');
+    evenMoreAds.forEach(ad => {
+      if (ad) {
+        ad.style.display = 'none';
+      }
+    });
   }
 });
